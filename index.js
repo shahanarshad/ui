c309194c6169d413cd44aa89b48940a45d310ff9
Shahan Arshad <shahan@Shahans-Mac-mini.local>
2022-09-19 06:16:01 +0500

    new commit

--- index.js
+++ index.js
@@ -2,26 +2,37 @@
 
 const express = require("express")
 
-//const expressEdge = require('express-edge');
+const expressEdge = require('express-edge');
 const path = require("path")
 const app= new express()
 
 app.use(express.static('public'))
-//app.use(expressEdge)
+app.use(expressEdge.engine)
 //app.set('views','${__dirname}/views')
+//app.use(express.static(path.join(__dirname + '../public')));
+//app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname+'/views');
 
-
-app.get('/index',(req,res)=> {     //routing for the home page
+/*app.get('/index',(req,res)=> {     //routing for the home page
 res.sendfile(path.resolve(__dirname,'pages/index.html'))
-})
-app.get('/home',(req,res)=> {     //routing for the home page
-    res.render('index')
+})*/
+/*app.get('/home',(req,res)=> {     //routing for the home page
+    res.render('index.html')
     })
 
-
-    app.get('/home',(req,res)=> {     //routing for the home page
+*/
+    /*app.get('/home',(req,res)=> {     //routing for the home page
         res.sendfile(path.resolve(__dirname,'pages/index.html'))
-        })
+        })*/
+
+app.get('/home',(req,res)=>{
+
+res.render('index')
+
+
+})
+
 
 app.get('/about',(req,res)=> {   // routing for the about page
     res.sendfile(path.resolve(__dirname,'pages/about.html'))
